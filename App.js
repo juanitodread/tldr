d2d0c34ab0dcda427e3e84bd312569571817fd40
Juan Sandoval <juanitodread@gmail.com>
2017-02-04 00:18:08 -0600

    Add create new article REST API

--- App.js
+++ App.js
@@ -1,13 +1,22 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
+
 const articles = [{title: 'Example'}];
+
+// Add support for body parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/articles', (req, res, next) => {
   res.send(articles);
 });
 
 app.post('/articles', (req, res, next) => {
-  res.send('OK');
+  const article = {title: req.body.title};
+  articles.push(article);
+  console.log(`New article added: ${article}`);
+  res.send(article);
 });
 
 app.get('/articles/:id', (req, res, next) => {
